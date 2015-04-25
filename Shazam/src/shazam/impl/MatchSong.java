c993210ad596b90e7fcca375a4c37fd3e2a8e053
Inonut <raducanu_dragos12@yahoo.com>
2015-04-26 00:58:23 +0300

    search YOUTUBE

---
+++ Shazam/src/shazam/impl/MatchSong.java
@@ -0,0 +1,13 @@
+package shazam.impl;
+
+import shazam.Song;
+
+public class MatchSong extends Song{
+
+	@Override
+	public void proccessSong() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ Shazam/src/shazam/impl/RegisterSong.java
@@ -0,0 +1,13 @@
+package shazam.impl;
+
+import shazam.Song;
+
+public class RegisterSong extends Song{
+
+	@Override
+	public void proccessSong() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
